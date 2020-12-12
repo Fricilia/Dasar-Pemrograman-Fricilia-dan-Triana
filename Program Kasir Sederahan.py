d6d65d649f2590e1db90a2b9787303e27b2b4a03
Fricilia <73948486+Fricilia@users.noreply.github.com>
2020-12-12 18:51:35 +0700

    Update Program Kasir Sederahan.py

--- Program Kasir Sederahan.py
+++ Program Kasir Sederahan.py
@@ -45,3 +45,31 @@ if nomor==3:
     total1=total1+porsi1*10000
     print ("Hasilnya = ", total1)
     jenis1=("Mie Ayam")
+
+def pilihan(i):
+        switcher={
+                1:'----Es Teh 3000----',
+                2:'----Es Jeruk 4000----',
+                3:'----Es Kopi 3000----'
+             }
+        return switcher.get(i,"Masukan Pilihan yang Benar!")
+print("\nMenu Minuman")
+print("1. Es teh")
+print("2. Es jeruk")
+print("3. Es kopi")
+nomor=int(input("Masukan Pilihan: "))
+menu=pilihan(nomor)
+print (menu)
+porsi2= int(input("Berapa Gelas: "))
+if nomor==1:
+    total2=total2+(porsi2*3000)
+    print ("Hasilnya = ", total2)
+    jenis2=("Es Teh")
+if nomor==2:
+    total2=total2+(porsi2*4000)
+    print ("Hasilnya = ", total2)
+    jenis2=("Es Jeruk")
+if nomor==3:
+    total2=total2+(porsi2*3000)
+    print ("Hasilnya = ", total2)
+    jenis2=("Es Kopi")
